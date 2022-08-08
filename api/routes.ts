08d325d02aaa059cc95274c902cc283b85cd81bf
Joe Auty <joe@datasprocket.com>
2022-08-07 19:20:35 -0700

    wc function

--- api/routes.ts
+++ api/routes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import {
-  getFiles, postFile, deleteFiles, mvFile,
+  getFiles, postFile, deleteFiles, mvFile, getWC,
 } from './streams';
 
 const router = express.Router();
 
 router.get('/file', getFiles); // healthcheck
 router.get('/file/:filename', getFiles);
+router.get('/file/:filename/wc', getWC);
 router.post('/file/:filename', postFile);
 router.put('/file/:filename', mvFile);
 router.delete('/file/:filename', deleteFiles);
